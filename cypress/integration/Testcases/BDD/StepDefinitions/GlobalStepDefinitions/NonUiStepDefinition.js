b5412675c1c4e5d9c75a9c9114977a220fc8eb48
Shubham <shubham_motwani@persistent.com>
2024-08-04 05:10:18 +0530

    First Commit

--- cypress/integration/Testcases/BDD/StepDefinitions/GlobalStepDefinitions/NonUiStepDefinition.js
+++ cypress/integration/Testcases/BDD/StepDefinitions/GlobalStepDefinitions/NonUiStepDefinition.js
@@ -4,11 +4,6 @@ const { When } = require("@badeball/cypress-cucumber-preprocessor");
 const { setContextData } = require("../../../../../utils/AutoContext.js");
 const pageHelper = new PageHelper();
 
-// Wait for "some" seconds
-When(/^Wait for "([^"]*)" seconds$/, function (seconds) {
-  cy.wait(seconds * 1000);
-});
-
 // Store Text "Test Text" in context "contextKey"
 When(
   /^Store Text "([^"]*)" in context "([^"]*)"$/,
